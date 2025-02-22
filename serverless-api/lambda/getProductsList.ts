68cc9e9e45614422370b74ace6b16ab932919096
KristinaKasyanovskaya <kristina_kasyanovskaya@epam.com>
2025-02-22 21:11:16 +0500

    fix: fix products

--- serverless-api/lambda/getProductsList.ts
+++ serverless-api/lambda/getProductsList.ts
@@ -1,10 +1,5 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-
-const products = [
-    { id: "1", name: "Product 1", price: 100 },
-    { id: "2", name: "Product 2", price: 200 },
-    { id: "3", name: "Product 3", price: 300 }
-];
+import { products } from "../products";
 
 export const handler: APIGatewayProxyHandler = async () => {
     return {
